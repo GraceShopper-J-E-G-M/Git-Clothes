cd11a1fc3c99788eb6324ff31b20233b31894750
Jessie Chiu <jessiechiu12@gmail.com>
2023-03-02 15:06:35 -0500

    pull updated model

--- client/app/store.js
+++ client/app/store.js
@@ -2,13 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import authReducer from "../features/auth/authSlice";
 import cartSlice from "../features/cart/cartSlice";
-import singleProductReducer from "../features/singleProduct/singleProductSlice";
+import singleProductSlice from "../features/singleProduct/singleProductSlice";
 import allProductSlice from "../features/allProducts/allProductSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    singleProduct: singleProductReducer,
+    singleProduct: singleProductSlice,
     cart: cartSlice,
     allProducts: allProductSlice,
   },
