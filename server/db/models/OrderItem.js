7b2080b622aafad5b5c6a9193fc0227d4f7910b9
grace-sanford <119084298+grace-sanford@users.noreply.github.com>
2023-03-02 14:59:42 -0500

    fixed orderItem

--- server/db/models/OrderItem.js
+++ server/db/models/OrderItem.js
@@ -1,20 +1,12 @@
 const Sequelize = require("sequelize");
 const db = require("../db");
 const OrderItem = db.define("orderItem", {
-  prodId: {
-    type: Sequelize.ARRAY,
-    allowNull: false,
-  },
-  selectedQuantity: {
+  quantity: {
     type: Sequelize.INTEGER,
     allowNull: false,
   },
   total: {
     type: Sequelize.DECIMAL,
-    allowNull: false,
-  },
-  cartId: {
-    type: Sequelize.ARRAY,
     allowNull: false,
   },
 });
