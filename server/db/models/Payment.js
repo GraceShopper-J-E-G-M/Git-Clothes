455365fc5067f044ee5477312e7716ce4d444623
msiva06 <m.sivasubramanian06@gmail.com>
2023-03-03 21:07:55 -0500

    added Payment model

---
+++ server/db/models/Payment.js
@@ -0,0 +1,19 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const Payment = db.define({
+  card: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  cvv: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  expiryYear: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Payment;
