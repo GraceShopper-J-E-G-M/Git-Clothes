36563a2d59d2b41fe2a1c367d93435eba261377a
msiva06 <m.sivasubramanian06@gmail.com>
2023-03-04 21:11:37 -0500

    added checkout conditions in address and checkout

--- client/features/checkout/Checkout.js
+++ client/features/checkout/Checkout.js
@@ -1,7 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchAddressAsync, selectAddress } from "./addressSlice";
-import { fetchCartAsync, selectCart } from "../cart/cartSlice";
+import {
+  fetchPaymentAsync,
+  addPaymentAsync,
+  selectPayment,
+} from "./paymentSlice";
+import {
+  fetchCartAsync,
+  updateCheckoutCartAsync,
+  selectCart,
+} from "../cart/cartSlice";
+import { addShippingAddressAsync, selectShipping } from "./shippingSlice";
 
 const Checkout = (props) => {
   const dispatch = useDispatch();
@@ -11,16 +21,84 @@ const Checkout = (props) => {
     if (user.id) {
       dispatch(fetchAddressAsync(user));
       dispatch(fetchCartAsync(user));
+      dispatch(fetchPaymentAsync(user));
     }
   }, [dispatch, user]);
 
   const addresses = useSelector(selectAddress);
   const cart = useSelector(selectCart);
-  console.log(cart);
+  const payment = useSelector(selectPayment);
+  const shipping = useSelector(selectShipping);
+  let totalCostWithTax;
+  console.log("In checkout address:", addresses);
+  console.log("In checkout cart", cart);
+  console.log("In checkout payment", payment);
+  console.log("In checkout shipping", shipping);
 
   const calculateTotalCost = (totalcost) => {
-    const totCostWithTax = Number(totalcost);
-    return Number(totCostWithTax + totCostWithTax * 0.05);
+    totalcost = Number(totalcost);
+    totalCostWithTax = totalcost + totalcost * 0.05;
+    return totalCostWithTax;
+  };
+
+  const [cardNum, setCardNum] = useState("");
+  const [cvv, setCvv] = useState("");
+  const [expiry, setExpiry] = useState("");
+
+  const handleFormPayment = (event) => {
+    if (event) event.preventDefault();
+    const reqbody = {
+      card: cardNum,
+      cvv,
+      expiryYear: expiry,
+    };
+    const cartId = cart.id;
+    dispatch(addPaymentAsync({ cartId, reqbody }));
+  };
+
+  const handlePayment = () => {
+    const reqbody = {
+      card: payment.card,
+      cvv: payment.cvv,
+      expiryYear: payment.expiryYear,
+    };
+    const cartId = cart.id;
+    dispatch(addPaymentAsync({ cartId, reqbody }));
+  };
+
+  const handleShippingAddress = () => {
+    const reqbody = {
+      line1: addresses.line1,
+      line2: addresses.line2,
+      zipcode: addresses.zipcode,
+      state: addresses.state,
+      city: addresses.city,
+      phoneNumber: addresses.phoneNumber,
+    };
+    const cartId = cart.id;
+    dispatch(addShippingAddressAsync({ cartId, reqbody }));
+  };
+
+  const handlePlaceOrder = () => {
+    const cartId = cart.id;
+    const cartProdList = cartProdListandQty();
+    const reqbody = {
+      totalCostWithTax,
+      cartId,
+      cartProdList,
+    };
+    dispatch(updateCheckoutCartAsync(reqbody));
+  };
+
+  const cartProdListandQty = () => {
+    const orderItems = cart.orderItems;
+    const prodList = orderItems.map((orderItem) => {
+      return {
+        prodId: orderItem.productId,
+        userQty: orderItem.quantity,
+      };
+    });
+    return prodList;
   };
 
   return (
@@ -34,6 +112,10 @@ const Checkout = (props) => {
           <p>State : {addresses.state}</p>
           <p>City : {addresses.city}</p>
           <p>Phone Number : {addresses.phoneNumber}</p>
+          <button type="button" onClick={handleShippingAddress}>
+            Use
+          </button>
+          <button type="button">Change Shipping Address</button>
         </div>
       ) : (
         <p>There is no shipping address available</p>
@@ -45,17 +127,39 @@ const Checkout = (props) => {
         <p>{cart.totalCartItems} items added for checkout</p>
       </div>
       <div>
-        <form>
+        <div>
+          <h3>Payment:</h3>
+          <p>Card : {payment.card}</p>
+          <p>Expiry year : {payment.expiryYear}</p>
+          <button type="button" onClick={handlePayment}>
+            Use
+          </button>
+        </div>
+        <form onSubmit={(event) => handleFormPayment(event)}>
           <label htmlFor="cardNumber">Card Number</label>
-          <input type="text" name="cardNumber" />
+          <input
+            type="text"
+            name="cardNumber"
+            onChange={(event) => setCardNum(event.target.value)}
+          />
           <label htmlFor="cvv">CVV</label>
-          <input type="text" name="cvv" />
+          <input
+            type="text"
+            name="cvv"
+            onChange={(event) => setCvv(event.target.value)}
+          />
           <label htmlFor="expiry">Expiry year</label>
-          <input type="text" name="expiry" />
-          <button type="button">save</button>
+          <input
+            type="text"
+            name="expiry"
+            onChange={(event) => setExpiry(event.target.value)}
+          />
+          <button type="submit">save</button>
         </form>
       </div>
-      <button type="button">Place order</button>
+      <button type="button" onClick={handlePlaceOrder}>
+        Place order
+      </button>
     </div>
   );
 };
