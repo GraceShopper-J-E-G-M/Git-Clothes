db72f3d6be81dda5a2c7bb84c5351b61fc342645
grace-sanford <119084298+grace-sanford@users.noreply.github.com>
2023-03-05 16:14:25 -0500

    Deleted commented out code from allProducts file

--- client/features/allProducts/allProducts.js
+++ client/features/allProducts/allProducts.js
@@ -1,12 +1,10 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchAllProductsAsync, selectAllProducts } from "./allProductSlice";
-import SingleProduct from "../singleProduct/singleProduct";
 
 const AllProducts = () => {
   const dispatch = useDispatch();
   const allProducts = useSelector(selectAllProducts);
-  // const { productId } = useParams();
 
   useEffect(() => {
     dispatch(fetchAllProductsAsync());
@@ -18,7 +16,6 @@ const AllProducts = () => {
       <div className="products">
         {allProducts.map((product) => {
           return (
-            // <SingleProduct key={product.id} id={product.id} product={product} />
             <div>
               <h2>{product.prodName}</h2>
               <img src={product.prodImg} />
