bf3f5813eb02641e8435acf6b27d3ce89e8a6664
grace-sanford <119084298+grace-sanford@users.noreply.github.com>
2023-04-05 16:36:14 -0400

    Commented app and admin folders

--- client/features/admin/allProducts/AdminAllProducts 2.js
+++ client/features/admin/allProducts/AdminAllProducts 2.js
@@ -1,54 +1,72 @@
+/**
+ * This file contains a `AdminAllProducts` component to display all products.
+ */
+
+//Libraries
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
-import { fetchAllProductsAsync, removeProduct, selectAllProducts } from "../../allProducts/allProductSlice";
+
+//Files
+import {
+  fetchAllProductsAsync,
+  removeProduct,
+  selectAllProducts,
+} from "../../allProducts/allProductSlice";
 import AddNewProduct from "./NewProduct";
 
+/**
+ * AdminAllProducts component.
+ */
 const AdminAllProducts = () => {
-    const products = useSelector(selectAllProducts);
+  const products = useSelector(selectAllProducts);
 
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-    //fetch data 
-    useEffect(() => {
-        dispatch(fetchAllProductsAsync());
-        setLoading(false);
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchAllProductsAsync());
+    setLoading(false);
+  }, [dispatch]);
 
-    const handleDeleteProduct = async (event, id) => {
-        event.preventDefault();
-        await dispatch(removeProduct(id));
-        await dispatch(fetchAllProductsAsync());
-        navigate("/allProducts");
-    };
+  const handleDeleteProduct = (event, id) => {
+    event.preventDefault();
+    dispatch(removeProduct(id));
+    dispatch(fetchAllProductsAsync());
+    navigate("/allProducts");
+  };
 
-    return loading ? (
-        <p style={{ textAlign: "center" }}>Loading...</p>
-    ) : (
-        // flex row
-        <div className="allDisplay">
-            <Link to="/admin">
-                <button>Back to admin portal</button>
-            </Link>
-            <section id="adminAllProducts">
-                {products && products.length
-                    ? products.map((product) => (
-                        <div key={product.id.toString()} className="adminProduct">
-                            <Link to={`/allProducts/${product.id}`}>
-                                <h2>{product.id} {product.prodName}</h2>
-                            </Link>
-                            <p>{`Price: ${product.prodPrice}`}</p>
-                            <button onClick={(event) => handleDeleteProduct(event, product.id)}>Delete Product</button>
-                        </div>
-                    ))
-                    : null}
-            </section>
-            <AddNewProduct />
-        </div>
-    )
-}
+  return loading ? (
+    <p style={{ textAlign: "center" }}>Loading...</p>
+  ) : (
+    <div className="allDisplay">
+      <Link to="/admin">
+        <button>Back to admin portal</button>
+      </Link>
+      <section id="adminAllProducts">
+        {products && products.length
+          ? products.map((product) => (
+              <div key={product.id.toString()} className="adminProduct">
+                <Link to={`/allProducts/${product.id}`}>
+                  <h2>
+                    {product.id} {product.prodName}
+                  </h2>
+                </Link>
+                <p>{`Price: ${product.prodPrice}`}</p>
+                <button
+                  onClick={(event) => handleDeleteProduct(event, product.id)}
+                >
+                  Delete Product
+                </button>
+              </div>
+            ))
+          : null}
+      </section>
+      <AddNewProduct />
+    </div>
+  );
+};
 
 export default AdminAllProducts;
--- client/features/admin/allProducts/NewProduct.js
+++ client/features/admin/allProducts/NewProduct.js
@@ -1,88 +1,143 @@
+/**
+ * This file contains a `AddNewProduct` component form for an admin to add a new product.
+ */
+
+//Libraries
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
+
+//Files
 import { newProduct } from "../../allProducts/allProductSlice";
 import { fetchAllProductsAsync } from "../../allProducts/allProductSlice";
 
+/**
+ * AddNewProduct component.
+ */
 const AddNewProduct = () => {
-    const sizeArray = ["XS", "S", "M", "L", "XL", "XXL"];
-    const colorArray = ["Red", "Pink", "Plum", "Mustard", "Burgundy", "Forest Green", "Beige", "Olive", "Grey", "Black", "Brown", "Dark Brown", "Blue"];
+  const sizeArray = ["XS", "S", "M", "L", "XL", "XXL"];
+  const colorArray = [
+    "Red",
+    "Pink",
+    "Plum",
+    "Mustard",
+    "Burgundy",
+    "Forest Green",
+    "Beige",
+    "Olive",
+    "Grey",
+    "Black",
+    "Brown",
+    "Dark Brown",
+    "Blue",
+  ];
 
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    //need state for storing edit
-    const [newProdName, setProdName] = useState("");
-    const [newProdQuantity, setProdQuantity] = useState(0);
-    const [newProdPrice, setProdPrice] = useState(0.00);
-    const [newProdSize, setProdSize] = useState("XS");
-    const [newProdColor, setProdColor] = useState("Red");
-    const [newProdImg, setProdImg] = useState("");
+  //Local state for storing a user's edits to the form
+  const [newProdName, setProdName] = useState("");
+  const [newProdQuantity, setProdQuantity] = useState(0);
+  const [newProdPrice, setProdPrice] = useState(0.0);
+  const [newProdSize, setProdSize] = useState("XS");
+  const [newProdColor, setProdColor] = useState("Red");
+  const [newProdImg, setProdImg] = useState("");
 
-    const handleAddProduct = async (event) => {
-        event.preventDefault();
-        //create an updated ProductObj to send to backend
-        const productObj = {
-            prodName: newProdName,
-            prodQuantity: newProdQuantity,
-            prodPrice: newProdPrice,
-            prodSize: newProdSize,
-            prodColor: newProdColor,
-            prodImg: newProdImg,
-        };
-        console.log(productObj);
-        await dispatch(newProduct(productObj));
-        setProdName("");
-        setProdQuantity(0);
-        setProdPrice(0.00);
-        setProdSize("XS");
-        setProdColor("Red");
-        setProdImg("");
-        await dispatch(fetchAllProductsAsync());
-        navigate("/allAdminProducts");
+  const handleAddProduct = (event) => {
+    event.preventDefault();
+    //Create an updated ProductObj to send to backend
+    const productObj = {
+      prodName: newProdName,
+      prodQuantity: newProdQuantity,
+      prodPrice: newProdPrice,
+      prodSize: newProdSize,
+      prodColor: newProdColor,
+      prodImg: newProdImg,
     };
+    dispatch(newProduct(productObj));
+    setProdName("");
+    setProdQuantity(0);
+    setProdPrice(0.0);
+    setProdSize("XS");
+    setProdColor("Red");
+    setProdImg("");
+    dispatch(fetchAllProductsAsync());
+    navigate("/allAdminProducts");
+  };
 
-    return (
-        <form id="addNewProduct"onSubmit={event => handleAddProduct(event)}>
-            <label>Product Name:
-                <input type="text" name="productName" value={newProdName}
-                    onChange={event => setProdName(event.target.value)} />
-            </label>
-            <label>Product Quantity: 
-                <input type="number" name="productQuantity" value={newProdQuantity}
-                    onChange={event => setProdQuantity(event.target.value)} />
-            </label>
-            <label>Product Price: 
-                <input type="number" step="0.01" name="productPrice" value={newProdPrice}
-                    onChange={event => setProdPrice(event.target.value)} />
-            </label>
-            <label>Product Size: 
-                <select name="prodSize"
-                    onChange={(event) => setProdSize(event.target.value)}>
-                    {sizeArray.map((size, index) => (
-                        <option value={size} key={index}>{size}</option>
-                    ))}
-                </select>
-            </label>
-            <label>Product Color: 
-                <select name="prodColor"
-                    onChange={(event) => setProdColor(event.target.value)}>
-                    {colorArray.map((color, index) => (
-                        <option value={color} key={index}>{color}</option>
-                    ))}
-                </select>
-            </label>
-            <label>Product Img: 
-                <input type="text" name="productImg" value={newProdImg}
-                    onChange={event => setProdImg(event.target.value)} />
-            </label>
-            <br></br>
-            <button className="submitNewProduct"type="submit">Update</button>
-        </form>
-    )
-}
+  return (
+    <form id="addNewProduct" onSubmit={(event) => handleAddProduct(event)}>
+      <label>
+        Product Name:
+        <input
+          type="text"
+          name="productName"
+          value={newProdName}
+          onChange={(event) => setProdName(event.target.value)}
+        />
+      </label>
+      <label>
+        Product Quantity:
+        <input
+          type="number"
+          name="productQuantity"
+          value={newProdQuantity}
+          onChange={(event) => setProdQuantity(event.target.value)}
+        />
+      </label>
+      <label>
+        Product Price:
+        <input
+          type="number"
+          step="0.01"
+          name="productPrice"
+          value={newProdPrice}
+          onChange={(event) => setProdPrice(event.target.value)}
+        />
+      </label>
+      <label>
+        Product Size:
+        <select
+          name="prodSize"
+          onChange={(event) => setProdSize(event.target.value)}
+        >
+          {sizeArray.map((size, index) => (
+            <option value={size} key={index}>
+              {size}
+            </option>
+          ))}
+        </select>
+      </label>
+      <label>
+        Product Color:
+        <select
+          name="prodColor"
+          onChange={(event) => setProdColor(event.target.value)}
+        >
+          {colorArray.map((color, index) => (
+            <option value={color} key={index}>
+              {color}
+            </option>
+          ))}
+        </select>
+      </label>
+      <label>
+        Product Img:
+        <input
+          type="text"
+          name="productImg"
+          value={newProdImg}
+          onChange={(event) => setProdImg(event.target.value)}
+        />
+      </label>
+      <br></br>
+      <button className="submitNewProduct" type="submit">
+        Update
+      </button>
+    </form>
+  );
+};
 
 export default AddNewProduct;
-
 
 //note for Jessie: remember inthe admin single product feature - add a field for Quantity
